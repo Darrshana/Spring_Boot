5d793be9fff0a1e99d52e680cb77c76bd2085650
Darrshana R <67387068+Darrshana@users.noreply.github.com>
2021-10-23 21:17:50 +0530

    basic webapp.

---
+++ webapp/src/main/java/com/darrshana/demo/homeController.java
@@ -0,0 +1,19 @@
+package com.darrshana.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class homeController {
+	
+@RequestMapping("home")
+public ModelAndView home(Person person) {
+	System.out.println("hi");
+	ModelAndView mv = new ModelAndView();
+	mv.addObject("obj",person);
+	mv.setViewName("home");
+	return mv;
+}
+}
---
+++ webapp/src/main/webapp/pages/home.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>WEBAPP</title>
+</head>
+<body>
+<h1>HI,I'm ${obj.pname} , id ${obj.pid} and I love ${obj.lang}.</h1>
+</body>
+</html>
